cbe9abaab2de30f79c4473f38a97af61dac0e149
Alejandro Garcia <garciaalejandro2552@gmail.com>
2025-04-24 02:16:07 -0500

    .gitignore added

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import fastapi
 import uvicorn
+from fastapi import FastAPI
 
+app = FastAPI(
+    title='New API',
+    description= 'API created with FASTAPI',
+    version='0.1.0'
+)
+
+@app.get('/')
+async def root():
+
+    """
+    Root endpoint
+    
+    """
+    return {"mensaje": "Welcome to my FastAPI API"}
